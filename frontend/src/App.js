ff20bdf7bb8a0393b79a131a0213eb614e604497
Tanya Agarwal <jiya03tanya@gmail.com>
2024-05-24 23:43:41 +0530

    Defined structure of frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div className="text-red">
-      App
+    <div className="">
+
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>}/>
+          <Route path="about" element={<h1>About</h1>}/>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
