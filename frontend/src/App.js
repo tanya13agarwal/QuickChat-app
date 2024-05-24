fac0d063e5eb4f625112b70626c5d8ec0de4764b
Tanya Agarwal <jiya03tanya@gmail.com>
2024-05-24 21:03:24 +0530

    Installed and setup Tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div>
+    <div className="text-red">
       App
     </div>
   );
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      red: 'rgb(255,0,0)',
+    }
+  },
+  plugins: [],
+}
