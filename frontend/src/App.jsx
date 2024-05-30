b0072d1d71dedd02c7130880538ec20a597d098f
Tanya Agarwal <135453589+tanya13agarwal@users.noreply.github.com>
2024-05-30 11:31:49 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,10 +30,11 @@ const App = () => {
 
   useEffect(() => {
     axios
-      .get(`${server}/api/v1/user/me`, { withCredentials: true })
+      .get(`https://quickchat-app-iouv.onrender.com/api/v1/user/me`, { withCredentials: true })
       .then(({ data }) => dispatch(userExists(data.user)))
       .catch((err) => dispatch(userNotExists()));
   }, [dispatch]);
+  // ${server}
 
   return loader ? (
     <LayoutLoader />
